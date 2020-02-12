b541918e6f621352fa1b3af7ad19f995b2000f5b
Chris Farmer <cfarmer@zipcodes-MacBook-Pro.local>
2020-02-12 15:38:23 -0500

    when in doubt hard code out

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,48 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    private Character[][] board;
     public Board(Character[][] matrix) {
+        this.board = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        return ((board[0][0].equals('X') && board[1][0].equals('X') && board[2][0].equals('X')) ||   //Across top
+                (board[0][1].equals('X') && board[1][1].equals('X') && board[2][1].equals('X')) ||   //Across middle
+                (board[0][2].equals('X') && board[1][2].equals('X') && board[2][2].equals('X')) ||   //Across bottom
+                (board[0][0].equals('X') && board[0][1].equals('X') && board[0][2].equals('X')) ||   //Down left
+                (board[1][0].equals('X') && board[1][1].equals('X') && board[1][2].equals('X')) ||   //Down middle
+                (board[2][0].equals('X') && board[2][1].equals('X') && board[2][2].equals('X')) ||   //Down right
+                (board[0][0].equals('X') && board[1][1].equals('X') && board[2][2].equals('X')) ||   //Diagonal bottom right to top left
+                (board[0][2].equals('X') && board[1][1].equals('X') && board[2][0].equals('X')));    //Diagonal bottom left to top right
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        return ((board[0][0].equals('O') && board[1][0].equals('O') && board[2][0].equals('O')) ||   //Across top
+                (board[0][1].equals('O') && board[1][1].equals('O') && board[2][1].equals('O')) ||   //Across middle
+                (board[0][2].equals('O') && board[1][2].equals('O') && board[2][2].equals('O')) ||   //Across bottom
+                (board[0][0].equals('O') && board[0][1].equals('O') && board[0][2].equals('O')) ||   //Down left
+                (board[1][0].equals('O') && board[1][1].equals('O') && board[1][2].equals('O')) ||   //Down middle
+                (board[2][0].equals('O') && board[2][1].equals('O') && board[2][2].equals('O')) ||   //Down right
+                (board[0][0].equals('O') && board[1][1].equals('O') && board[2][2].equals('O')) ||   //Diagonal bottom right to top left
+                (board[0][2].equals('O') && board[1][1].equals('O') && board[2][0].equals('O')));    //Diagonal bottom left to top right
     }
 
     public Boolean isTie() {
-        return null;
+        return (this.isInFavorOfO() == this.isInFavorOfX());
     }
 
     public String getWinner() {
-        return null;
+        if (this.isInFavorOfO()){
+            return "O";
+        }
+        else if (this.isInFavorOfX()){
+            return "X";
+        }
+        else {
+            return "";
+        }
     }
 
 }
